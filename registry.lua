f4dff5d360c1b98e3a097488a3da7c394b2d0c0e
notifierfree9-lang <notifierfree9-lang@users.noreply.github.com>
2026-08-08 23:33:04 +0500

    Add game IDs for registry script 3

--- registry.lua
+++ registry.lua
@@ -6,6 +6,10 @@ return {
 
         ["96342491571673, 109983668079237, 128762245270197, 99606176102979"] = [[
             loadstring(game:HttpGet("https://raw.githubusercontent.com/notifierfree9-lang/nameless-remote-registry-test/refs/heads/main/2", true))()
+        ]],
+
+        ["97598239454123, 73504898027860"] = [[
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/notifierfree9-lang/nameless-remote-registry-test/refs/heads/main/3", true))()
         ]]
     }
 }
