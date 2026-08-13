28152ae1c0823feb02649d19ef110f96a490fcf4
notifierfree9-lang <notifierfree9@gmail.com>
2026-08-13 19:58:50 +0500

    Update registry.lua

--- registry.lua
+++ registry.lua
@@ -1,14 +1,14 @@
 return {
-    places = {
-        [104522435597696] = [[
+    games = {
+        [10148749921] = [[
             loadstring(game:HttpGet("https://raw.githubusercontent.com/notifierfree9-lang/nameless-remote-registry-test/refs/heads/main/1", true))()
         ]],
 
-        ["96342491571673, 109983668079237, 128762245270197, 99606176102979"] = [[
+        [7709344486] = [[
             loadstring(game:HttpGet("https://raw.githubusercontent.com/notifierfree9-lang/nameless-remote-registry-test/refs/heads/main/2", true))()
         ]],
 
-        ["97598239454123, 73504898027860, 77085202503540, 129343810645058"] = [[
+        [10200395747] = [[
             loadstring(game:HttpGet("https://raw.githubusercontent.com/notifierfree9-lang/nameless-remote-registry-test/refs/heads/main/3", true))()
         ]]
     }
