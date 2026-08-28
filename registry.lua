be1e78e462d3675856a0a45c91cea16aeb77a0b4
notifierfree9-lang <notifierfree9-lang@users.noreply.github.com>
2026-08-28 20:11:15 +0500

    Clean repository history

---
+++ registry.lua
@@ -0,0 +1,19 @@
+return {
+    games = {
+        [10148749921] = [[
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/notifierfree9-lang/nameless-remote-registry-test/refs/heads/main/1", true))()
+        ]],
+
+        [7709344486] = [[
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/notifierfree9-lang/nameless-remote-registry-test/refs/heads/main/2", true))()
+        ]],
+
+        [10200395747] = [[
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/notifierfree9-lang/nameless-remote-registry-test/refs/heads/main/3", true))()
+        ]],
+
+        [7326934954] = [[
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/notifierfree9-lang/nameless-remote-registry-test/refs/heads/main/4", true))()
+        ]]
+    }
+}
